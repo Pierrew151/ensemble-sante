75bf583c0b2b2e0f6e8d06d8650b306e4b48b095
Pierrew151 <p.weber145@laposte.net>
2022-03-02 10:42:49 +0100

    Initial commit

---
+++ src/main/java/fr/ensemblesante/EnsembleSanteApplication.java
@@ -0,0 +1,15 @@
+package fr.ensemblesante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class EnsembleSanteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsembleSanteApplication.class, args);
+    }
+
+}
---
+++ src/main/java/fr/ensemblesante/bll/UserRepository.java
@@ -0,0 +1,12 @@
+package fr.ensemblesante.bll;
+
+import org.springframework.data.repository.CrudRepository;
+
+import fr.ensemblesante.bo.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
